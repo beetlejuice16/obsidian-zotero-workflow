94c3ca692d50116d30a2183be201798bd612b954
Fayez <fayez.bajjali@proton.me>
2024-06-15 16:28:06 +0200

    Try new setup for proper css loading

--- next.config.js
+++ next.config.js
@@ -2,8 +2,20 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx'
 })
- 
-module.exports = {...withNextra({output: 'export'}), images: { unoptimized: true}}
+
+/**
+* @type {import('next').NextConfig}
+*/
+const nextConfig = {
+  output: "export",
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  assetPrefix: "./",
+};
+
+module.exports = {...withNextra({output: 'export'}), ...nextConfig}
 
  
 // If you have other Next.js configurations, you can pass them as the parameter:
